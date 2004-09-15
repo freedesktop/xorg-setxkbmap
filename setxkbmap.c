04cdd8bc75f7de88b8725509546af37b4afb49c6
Alexander Gottwald <alexander.gottwald@s1999.tu-chemnitz.de>
2004-09-15 16:34:16 +0000

    Pull XORG-6_8_0 to CYGWIN branch

--- setxkbmap.c
+++ setxkbmap.c
@@ -1,4 +1,4 @@
-/* $XdotOrg: xc/programs/setxkbmap/setxkbmap.c,v 3.7 2003/01/20 04:15:08 dawes Exp $ */
+/* $XdotOrg: xc/programs/setxkbmap/setxkbmap.c,v 1.2 2004/04/23 19:54:37 eich Exp $ */
 /************************************************************
  Copyright (c) 1996 by Silicon Graphics Computer Systems, Inc.
 
